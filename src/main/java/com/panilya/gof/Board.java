4d1ebaa658856442ba7c75e42a216a3b46f813f2
panilya <panilyau@gmail.com>
2022-02-22 19:26:52 +0200

    Added the main logic and made the prototype working

--- src/main/java/com/panilya/gof/Board.java
+++ src/main/java/com/panilya/gof/Board.java
@@ -3,7 +3,7 @@ package com.panilya.gof;
 public class Board {
     private final int x;
     private final int y;
-    private final int[][] board;
+    private int[][] board;
 
     public Board(int x, int y) {
         this.board = new int[x][y];
@@ -27,27 +27,87 @@ public class Board {
         }
     }
 
-    public boolean alive(int x, int y) {
-        if (board[x][y] == 1) {
-            return true;
+    public void setAlive(int x, int y) {
+        board[x][y] = 1;
+    }
+
+    public void setDead(int x, int y) {
+        board[x][y] = 0;
+    }
+
+    private int countNeighbours(int x, int y) {
+        int count = 0;
+
+        if (x != 0) {
+            if (alive(x-1, y)) count++;
         }
-        return false;
+        if (y != 0) {
+            if (alive(x, y-1)) count++;
+        }
+        if (x != 0 && y != 0) {
+            if (alive(x-1,y-1)) count++;
+        }
+        if (y != this.y-1) {
+            if (alive(x, y+1)) count++;
+        }
+        if (x != this.x - 1) {
+            if (alive(x+1, y)) count++;
+        }
+        if (x != this.x - 1 && y != this.y - 1) {
+            if (alive(x+1, y+1)) count++;
+        }
+        if (x != this.x - 1 && y != 0) {
+            if (alive(x+1, y-1)) count++;
+        }
+        if (x != 0 && y != this.y-1) {
+            if (alive(x-1,y+1)) count++;
+        }
+        return count;
+    }
+
+    public void step() {
+        int[][] newBoard = new int[this.x][this.y];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+
+                int neighbours = countNeighbours(i, j);
+                if (board[i][j] == 1) {
+                    if (neighbours == 0 || neighbours == 1) {
+                        newBoard[i][j] = 0;
+                    }
+                    if (neighbours == 2 || neighbours == 3) {
+                        newBoard[i][j] = 1;
+                    }
+                    if (neighbours > 3) {
+                        newBoard[i][j] = 0;
+                    }
+                } else {
+                    if (neighbours == 3) {
+                        newBoard[i][j] = 1;
+                    }
+                }
+            }
+        }
+        this.board = newBoard;
+    }
+
+    public boolean alive(int x, int y) {
+        return board[x][y] == 1;
     }
 
     public void renderBoard() {
-        System.out.println("_".repeat(x+2));
+        System.out.println("_".repeat(x));
         for (int i = 0; i < board.length; i++) {
             System.out.print("|");
             for (int j = 0; j < board[i].length; j++) {
                 if (board[i][j] == 1) {
-                    System.out.print("X");
+                    System.out.print("O");
                 } else {
                     System.out.print(".");
                 }
             }
             System.out.println("|");
         }
-        System.out.println("-".repeat(x+2));
-
+        System.out.println("-".repeat(x));
     }
 }
--- src/main/java/com/panilya/gof/GameOfLife.java
+++ src/main/java/com/panilya/gof/GameOfLife.java
@@ -2,8 +2,23 @@ package com.panilya.gof;
 
 public class GameOfLife {
     public static void main(String[] args) {
-        Board board = new Board(5,5);
-        board.initBoard();
-        board.renderBoard();
+        Board board = new Board(6,4);
+        board.setAlive(2,2);
+        board.setAlive(3,2);
+        board.setAlive(4,2);
+
+
+        // board.initBoard();
+
+        for (int i = 0; i < 5; i++) {
+            board.renderBoard();
+            board.step();
+        }
+//
+//        board.renderBoard();
+//        board.step();
+//        board.renderBoard();
+//        board.step();
+//        board.renderBoard();
     }
 }
